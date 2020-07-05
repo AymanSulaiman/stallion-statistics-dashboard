de9d246339a705429bb291849add93e63287d49a
AymanSulaiman <saymansulaiman@gmail.com>
2020-07-04 17:31:38 -0700

    the tableau stuff is in the dashboard

--- Dash-website/app.py
+++ Dash-website/app.py
@@ -3,6 +3,7 @@ import dash
 import os
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
@@ -11,13 +12,27 @@ import ml_outputs as mo
 from dash.dependencies import Input, Output
 from notebook_html import notebook
 import dash_bootstrap_components as dbc
+import tableau_graphs as tg
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}])
 
 df = pd.read_csv('cleaned_tvg_cd_data.csv')
+
+display_df = df[
+    ['horsename',
+     'tvghorseweight',      
+     'tvghorseage',         
+     'tvghorsedaysoff',     
+     'tvghorsenumberofwins',
+     'tvghorsepowerrating',
+     'tvghorseaverageclassrating',
+     'tvghorseaveragespeed',
+     'winpayoutbinary']
+]
 
 features = [
     'tvghorseweight',            # 0
@@ -55,6 +70,27 @@ fig_example.update_layout(
             )
 )
 
+fig_example_1 = go.Figure()
+
+fig_example_1.add_trace(go.Scatter(x=x, y=y,
+                    mode='markers',
+                    name='power, speed'))
+
+fig_example_1.add_trace(go.Scatter(x=x, y=(x*m)+c,
+                    mode='lines',
+                    name='Line of best fit'))
+
+fig_example_1.update_layout(
+            title="Power and speed of horse",
+            xaxis_title="Power",
+            yaxis_title="Speed",
+            font=dict(
+                family="Courier New, monospace",
+                size=16,
+                color="#7f7f7f"
+            )
+)
+
 def generate_table(dataframe, max_rows=10):
     return html.Table([
         html.Thead(
@@ -65,8 +101,7 @@ def generate_table(dataframe, max_rows=10):
                 html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
             ]) for i in range(min(len(dataframe), max_rows))
         ])
-    ])
-
+    ], style={'textAlign': 'center'})
 
 
 app.layout = html.Div(children=[
@@ -80,42 +115,58 @@ app.layout = html.Div(children=[
         Built in Python, made with ❤️
     '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig_example
-    ),
+
+    html.Div([
+
+        html.Div([
+            html.H3('column 1'),
+            dcc.Graph(
+                id='example-graph-1',
+                figure=fig_example_1
+            )
+        ], className="pretty_container six columns"),
+
+        html.Div([
+            html.H3('column 2'),
+            dcc.Graph(
+                id='example-graph',
+                figure=fig_example
+            )
+        ], className="pretty_container six columns")
 
 
-    html.H4(children='horse racing data'),
-    generate_table(df),
+    ], className="flex-display row pretty-container"),
 
-    html.Iframe(srcDoc=notebook,style=dict(border=0), width="100%", height="800")
-])
+
+    html.H4(children='horse racing data_1'),
+    generate_table(display_df, max_rows=10),
+
+    html.Div([
+        html.Div([
+            html.Iframe(srcDoc=tg.daily_winpayout_beyer_speed, width="100%", height="587", style={'border':0}),  
+            html.Iframe(srcDoc=tg.horse_racing_dashboard, width="100%", height="587",style={'border':0})
+        ]),
+
+        html.Div([
+            html.Iframe(srcDoc=tg.avg_horse_power, width="100%", height="587",style={'border':0})
+        ]),
+
+        
+        # html.Iframe(srcDoc=notebook,style=dict(border=0), width="100%", height="800")
+    ]),
+
+    html.Div([
+        html.Div([
+
+        ]),
+
+        html.Div([
+            
+        ])
+    ])
+], style={'textAlign': 'center'})
 
 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-# assume you have a "wide-form" data frame with no index
-# see https://plotly.com/python/wide-form/ for more options
-# df = pd.DataFrame({"x": [1, 2, 3], "SF": [4, 1, 2], "Montreal": [2, 4, 5]})
-
-# fig = px.bar(df, x="x", y=["SF", "Montreal"], barmode="group")
-
-# app.layout = html.Div(children=[
-#     html.H1(children='Hello Dash'),
-
-#     html.Div(children='''
-#         Dash: A web application framework for Python.
-#     '''),
-
-#     dcc.Graph(
-#         id='example-graph',
-#         figure=fig
-#     )
-# ])
-
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
---
+++ Dash-website/tableau_graphs.py
@@ -0,0 +1,133 @@
+daily_winpayout_beyer_speed = '''
+<div class='tableauPlaceholder' id='viz1593903068778' style='position: relative'>
+<noscript>
+<a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Fi&#47;FinalProject-DataAnalyticsBootcamp&#47;Dashboard2&#47;1_rss.png' style='border: none' />
+
+</a>
+</noscript>
+
+<object class='tableauViz'  style='display:none;'>
+    <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
+    <param name='embed_code_version' value='3' /> 
+    <param name='site_root' value='' />
+    <param name='name' value='FinalProject-DataAnalyticsBootcamp&#47;Dashboard2' />
+    <param name='tabs' value='no' /><param name='toolbar' value='yes' />
+    <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Fi&#47;FinalProject-DataAnalyticsBootcamp&#47;Dashboard2&#47;1.png' /> 
+    <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' />
+    <param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-GB' />
+</object>
+</div>                
+
+<script type='text/javascript'>                    
+    var divElement = document.getElementById('viz1593903068778');                    
+    var vizElement = divElement.getElementsByTagName('object')[0];                    
+    
+    if ( divElement.offsetWidth > 800 ) { 
+        vizElement.style.minWidth='100vw';
+        vizElement.style.maxWidth='100%';
+        vizElement.style.minHeight='587px';
+        vizElement.style.maxHeight=(divElement.offsetWidth*0.75)+'px';} 
+    else if ( divElement.offsetWidth > 500 ) { 
+        vizElement.style.minWidth='420px';
+        vizElement.style.maxWidth='100%';
+        vizElement.style.minHeight='587px';
+        vizElement.style.maxHeight=(divElement.offsetWidth*0.75)+'px';
+    } 
+    else { 
+        vizElement.style.width='100%';
+        vizElement.style.height='777px';
+    }                     
+    var scriptElement = document.createElement('script');                    
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);                
+</script>
+'''
+
+horse_racing_dashboard = '''
+<div class='tableauPlaceholder' id='viz1593897195021' style='position: relative'>
+
+<noscript>
+<a href='#'>
+    <img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Fi&#47;FinalProject-DataAnalyticsBootcamp&#47;Dashboard1&#47;1_rss.png' style='border: none' />
+</a>
+</noscript>
+<object class='tableauViz'  style='display:none;'>
+    <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+    <param name='embed_code_version' value='3' /> 
+    <param name='site_root' value='' />
+    <param name='name' value='FinalProject-DataAnalyticsBootcamp&#47;Dashboard1' />
+    <param name='tabs' value='no' /><param name='toolbar' value='yes' />
+    <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Fi&#47;FinalProject-DataAnalyticsBootcamp&#47;Dashboard1&#47;1.png' /> 
+    <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' />
+    <param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' />
+    <param name='display_count' value='yes' /><param name='language' value='en-GB' />
+</object>
+
+</div>
+
+<script type='text/javascript'>
+    
+    var divElement = document.getElementById('viz1593897195021');                    
+    
+    var vizElement = divElement.getElementsByTagName('object')[0];                    
+    
+    if ( divElement.offsetWidth > 800 ) { 
+        vizElement.style.minWidth='420px';
+        vizElement.style.maxWidth='2100px';
+        vizElement.style.width='100%';
+        vizElement.style.minHeight='587px';
+        vizElement.style.maxHeight='1387px';
+        vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
+    } 
+    
+    else if ( divElement.offsetWidth > 500 ) { 
+        vizElement.style.minWidth='420px';
+        vizElement.style.maxWidth='2100px';
+        vizElement.style.width='100%';
+        vizElement.style.minHeight='587px';
+        vizElement.style.maxHeight='1387px';
+        vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
+    } 
+    
+    else { 
+        vizElement.style.width='100%';
+        vizElement.style.height='1477px';
+    }                     
+    
+    var scriptElement = document.createElement('script');                    
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);                
+</script>
+'''
+
+avg_horse_power = '''
+<div class='tableauPlaceholder' id='viz1593906275001' style='position: relative'>
+<noscript>
+<a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Fi&#47;FinalProject-DataAnalyticsBootcamp&#47;AvgPowerRatingRanking&#47;1_rss.png' style='border: none' />
+</a>
+</noscript>
+<object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+    <param name='embed_code_version' value='3' /> 
+    <param name='site_root' value='' />
+    <param name='name' value='FinalProject-DataAnalyticsBootcamp&#47;AvgPowerRatingRanking' />
+    <param name='tabs' value='no' /><param name='toolbar' value='yes' />
+    <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Fi&#47;FinalProject-DataAnalyticsBootcamp&#47;AvgPowerRatingRanking&#47;1.png' /> 
+    <param name='animate_transition' value='yes' />
+    <param name='display_static_image' value='yes' />
+    <param name='display_spinner' value='yes' />
+    <param name='display_overlay' value='yes' />
+    <param name='display_count' value='yes' />
+    <param name='language' value='en-GB' />
+</object>
+</div>                
+
+<script type='text/javascript'>                    
+    var divElement = document.getElementById('viz1593906275001');                    
+    var vizElement = divElement.getElementsByTagName('object')[0];                    
+    vizElement.style.width='100%';
+    vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    
+    var scriptElement = document.createElement('script');                    
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);                
+</script>
+'''
